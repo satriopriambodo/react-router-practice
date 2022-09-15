c391f07c4f4096f8ac901f9bdb7379bc2a245dd5
satrio priambodo <satriopriambodo96@gmail.com>
2022-09-15 14:11:19 +0700

    sorting quotes

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Satrio", text: "Learning is fun!" },
+  { id: "q2", author: "Priambodo", text: "Hello World!" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quote Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
